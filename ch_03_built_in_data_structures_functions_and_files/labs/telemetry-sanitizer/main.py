12a2ecb7e2c7c8ee6c3f3dcb9e0469defbd316dd
Ciro J <cirojunioteixeira1@gmail.com>
2026-04-21 16:54:27 -0300

    feat(lab-telemetry-sanitizer): Create a function that processes the lines.

--- ch_03_built_in_data_structures_functions_and_files/labs/telemetry-sanitizer/main.py
+++ ch_03_built_in_data_structures_functions_and_files/labs/telemetry-sanitizer/main.py
@@ -1,13 +1,19 @@
 from src.drivers.io_handler import stream_telemetry
 from src.core.processor import aggregate_peaks
+from src.core.processor import process_signal
 
 def run_telemetry_system():
-    path = 'data/telemetry.log' # Caminho relativo à raiz [cite: 970, 971]
+    path = 'data/telemetry.log'
     
-    # Inicia o fluxo (Generator) [cite: 783, 796]
+    # Inicia o fluxo (Generator) 
     stream = stream_telemetry(path)
     
-    # Processa os dados de forma preguiçosa (Lazy Evaluation) [cite: 363, 801]
+    process =  process_signal(stream)
+
+    for _ in range(10):
+        print(process[_])
+
+    # Processa os dados de forma preguiçosa (Lazy Evaluation) 
     result = aggregate_peaks(stream)
     
     print(f"Resultado Final: {result}")
--- ch_03_built_in_data_structures_functions_and_files/labs/telemetry-sanitizer/src/core/processor.py
+++ ch_03_built_in_data_structures_functions_and_files/labs/telemetry-sanitizer/src/core/processor.py
@@ -10,6 +10,25 @@ def aggregate_peaks(data_stream):
     """
     Utiliza Sets para unicidade e Dicts para picos de temperatura.
     Retorna: (total_unicos, dict_picos)
+
+
+
     """
     # TODO: Implementar usando apenas o stream (iteração única)
     pass
+
+def process_signal(raw_signal): 
+    # print(f"DEBUG: Objeto Ingressante ID: {id(raw_signal)}")
+
+    if raw_signal is None:
+        return []
+    
+    if isinstance(raw_signal, str):
+        return [raw_signal.strip()]
+    
+    try:
+        iterator = iter(raw_signal)
+        # Duck Typing: Uma nova lista mutável
+        return [item.strip() if isinstance(item, str) else item for item in iterator]
+    except TypeError:
+        return [raw_signal]
